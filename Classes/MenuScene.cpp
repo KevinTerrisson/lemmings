62a0cf9c97f791b13ceca520024df62a4f10dd31
ataillat <ataillat@gaming.tech>
2022-12-16 09:53:35 +0100

    bouton et font

--- Classes/MenuScene.cpp
+++ Classes/MenuScene.cpp
@@ -15,17 +15,18 @@ bool MenuScene::init() {
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+    ////// Image de fond //////
     auto background = Sprite::create("res/background.png");
     background->setPosition(Vec2(visibleSize.width/2 + origin.x , visibleSize.height/2 + origin.y));
+    this->addChild(background);
 
-
+    ////// Images des boutons //////
     startMenu = MenuItemImage::create("res/jouer.png", "res/jouer-select.png", CC_CALLBACK_1(MenuScene::changeScene, this));
     exitMenu = MenuItemImage::create("res/quitter.png", "res/quitter-select.png", CC_CALLBACK_1(MenuScene::exitGame, this));
-
     auto menu = Menu::create(startMenu, exitMenu, nullptr);
     menu->setPosition(Point::ZERO);
-    startMenu->setPosition(visibleSize.width / 3 + origin.x, visibleSize.height / 3 + origin.y);
-    exitMenu->setPosition((visibleSize.width / 3 + origin.x) * 2, visibleSize.height / 3 + origin.y); //-exitMenu->getContentSize().height - 20);
+    startMenu->setPosition((visibleSize.width / 6 + origin.x) * 3.5, visibleSize.height / 5 + origin.y);
+    exitMenu->setPosition((visibleSize.width / 6 + origin.x) * 5, visibleSize.height / 5 + origin.y); //-exitMenu->getContentSize().height - 20);
     this->addChild(menu);
 
     return true;
