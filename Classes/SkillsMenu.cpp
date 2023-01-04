19d650f3a6ebab9d08eb0cb3899c39b26c925afc
ataillat <ataillat@gaming.tech>
2023-01-04 17:22:28 +0100

    bouton

--- Classes/SkillsMenu.cpp
+++ Classes/SkillsMenu.cpp
@@ -22,7 +22,7 @@ void SkillsMenu::loadSkillsMenu()
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     //auto Back = Sprite::create("res/SkillsMenuBackground");
-    auto Bomb = MenuItemImage::create("res/BombButton.png", "res/BombButton.png", CC_CALLBACK_1(SkillsMenu::OnClick, this));
+    auto Bomb = MenuItemImage::create("res/BombButton.png", "res/BombButton2.png", CC_CALLBACK_1(SkillsMenu::OnClick, this));
 
     auto menu = Menu::create(Bomb, /*Back, */ nullptr);
   
@@ -31,24 +31,5 @@ void SkillsMenu::loadSkillsMenu()
 
 void SkillsMenu::OnClick(Ref* pSender)
 {
-    // changing cursor icon
-    /*
-    auto director = cocos2d::Director::getInstance();
-    auto cursorSprite = cocos2d::Sprite::create("myCursor.png");
-    director->getOpenGLView()->setCursor(cursorSprite);
-    */
-
-    auto director = cocos2d::Director::getInstance();
-    auto cursorSprite = cocos2d::Sprite::create("myCursor.png");
-
-    auto mouseListener = cocos2d::EventListenerMouse::create();
-    mouseListener->onMouseMove = [](cocos2d::EventMouse* event) {
-        // Récupérez la position du curseur
-        cocos2d::Vec2 cursorPos = event->getLocation();
-        // Déplacez le sprite de curseur à la position du curseur
-        cursorSprite->setPosition(cursorPos);
-    };
-
-    // Ajoutez le gestionnaire d'événements de la souris à la file d'attente d'événements
-    director->getEventDispatcher()->addEventListenerWithSceneGraphPriority(mouseListener, this);
+    //action
 }
